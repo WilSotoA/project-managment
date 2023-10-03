f0b4412839cecdd59f7fd3a4e3e8b524000272d4
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-10-03 14:34:58 -0500

    complete-auth-routes

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -6,24 +6,48 @@ use Illuminate\Http\Request;
 use App\Http\Requests\UserStoreRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\QueryException;
 
 class AuthController extends Controller
 {
     public function store(UserStoreRequest $request)
     {
-        $data = $request->validated();
+        try {
+            $data = $request->validated();
 
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt( $data['password'] )
-        ]);
-
-        return response()->json(['message' => 'Usuario creado exitosamente!']);
+            $user = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => bcrypt($data['password'])
+            ]);
+            return response()->json(['message' => 'Usuario creado exitosamente!']);
+        } catch (\Exception $e) {
+            $errorCode = $e->getCode();
+            if ($errorCode === '23000') {
+                return response()->json(['message' => 'El correo electrónico ya está en uso.'], 400);
+            }
+            return response()->json(['error' => $errorCode, 'message' => 'Error al crear el usuario.'], 500);
+        }
     }
 
-    public function login()
+    public function login(Request $request)
     {
-        return 'hello';
+        try {
+            $credentials = $request->validate([
+                'email' => ['required', 'email'],
+                'password' => ['required'],
+            ]);
+
+            if (Auth::attempt($credentials)) {
+                return response()->json(['login' => true]);
+            }
+            return response()->json(['login' => false, 'message' => 'Usuario o contraseña incorrectos'], 401);
+        } catch (\Exception $e) {
+            $errorCode = $e->getCode();
+            if ($errorCode === '23000') {
+                return response()->json(['message' => 'El correo electrónico ya está en uso.'], 400);
+            }
+            return response()->json(['error' => $errorCode, 'message' => 'Error al iniciar sesión'], 500);
+        }
     }
 }
