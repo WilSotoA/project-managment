e086085af88980f47e193f87b3ca6e25e95f3f8b
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-10-03 18:37:08 -0500

    config-client-with-vite-vue-and-tailwind

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class="text-blue-600">hola mundo</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
